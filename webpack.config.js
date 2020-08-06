634cb65461a3c1cea61485730429b5c36797785f
jhonatanquirama <quiramapino47@gmail.com>
2020-08-06 12:44:27 -0500

    actualizaciones

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/app/index.js",
+  output: {
+    filename: "app.bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+};
